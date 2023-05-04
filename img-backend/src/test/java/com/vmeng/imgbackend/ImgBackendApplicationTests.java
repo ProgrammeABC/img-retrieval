66acf8c77f3401f27fbe0200e50f8fbb90ad757d
micromsg <micromsg6@outlook.com>
2023-05-04 17:27:43 +0800

    初始化

---
+++ img-backend/src/test/java/com/vmeng/imgbackend/ImgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmeng.imgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
